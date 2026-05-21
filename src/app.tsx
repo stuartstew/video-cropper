623558d517e390df0a6341d7e9ea8fbb1a95bc84
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-05-21 19:03:28 +0900

    feat: add save button

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import "@mantine/core/styles.css";
 import { Box, MantineProvider, Stack } from "@mantine/core";
 import { FrameCrop } from "./components/frame-crop";
 import { ROIInput } from "./components/roi-input";
+import { SaveButton } from "./components/save-button";
 import { VideoInput } from "./components/video-input";
 import { useCrop } from "./hooks/use-crop";
 
@@ -11,10 +12,10 @@ const App = () => {
 
   return (
     <MantineProvider defaultColorScheme="dark">
-      <Stack h="100vh" display="flex">
+      <Stack h="100vh" display="flex" gap={0}>
         <VideoInput />
         <FrameCrop />
-        <Box px="xl" py="md">
+        <Box px="xl" pt="md">
           <ROIInput
             x={x}
             y={y}
@@ -25,6 +26,9 @@ const App = () => {
             onChangeWidth={changeWidth}
             onChangeHeight={changeHeight}
           />
+        </Box>
+        <Box px="xl" pt="md" pb="md">
+          <SaveButton />
         </Box>
       </Stack>
     </MantineProvider>
---
+++ src/components/save-button.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@mantine/core";
+
+export const SaveButton = () => {
+  return <Button>Save</Button>;
+};
