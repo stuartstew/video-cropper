2bdf29c7410fa096545bcd5e427df51eef49fee0
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-05-21 20:21:09 +0900

    test: setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -29,4 +29,10 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.mjs",
+  },
 }));
