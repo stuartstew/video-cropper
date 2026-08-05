406b7b6a861ea5699b95e03653381c8552e88364
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-08-05 18:34:54 +0900

    test: split test for pixel crop into multiple tests

--- src/hooks/use-crop.test.ts
+++ src/hooks/use-crop.test.ts
@@ -3,22 +3,39 @@ import { act, renderHook } from "@/test-utils";
 import { useCrop } from "./use-crop";
 
 describe("use-crop", () => {
-  it("should update crop", () => {
+  it("should update pixel crop", () => {
     const { result } = renderHook(() => useCrop({ width: 600, height: 400 }));
 
     act(() => result.current.changePixelCrop({ unit: "px", x: 0, y: 100, width: 300, height: 200 }));
     expect(result.current.pixelCrop).toEqual({ unit: "px", x: 0, y: 100, width: 300, height: 200 });
+  });
 
-    act(() => result.current.changePixelCrop({ ...result.current.pixelCrop, height: 400 }));
-    expect(result.current.pixelCrop.height).toBe(300);
+  it("should clamp x of pixel crop", () => {
+    const { result } = renderHook(() => useCrop({ width: 600, height: 400 }));
 
-    act(() => result.current.changePixelCrop({ ...result.current.pixelCrop, y: 350 }));
-    expect(result.current.pixelCrop.y).toBe(350);
-    expect(result.current.pixelCrop.height).toBe(50);
+    act(() => result.current.changePixelCrop({ unit: "px", x: 600, y: 100, width: 300, height: 200 }));
+    expect(result.current.pixelCrop).toEqual({ unit: "px", x: 599, y: 100, width: 1, height: 200 });
+  });
 
-    act(() => result.current.changePixelCrop({ ...result.current.pixelCrop, x: 600 }));
-    expect(result.current.pixelCrop.x).toBe(599);
-    expect(result.current.pixelCrop.width).toBe(1);
+  it("should clamp y of pixel crop", () => {
+    const { result } = renderHook(() => useCrop({ width: 600, height: 400 }));
+
+    act(() => result.current.changePixelCrop({ unit: "px", x: 0, y: 400, width: 300, height: 200 }));
+    expect(result.current.pixelCrop).toEqual({ unit: "px", x: 0, y: 399, width: 300, height: 1 });
+  });
+
+  it("should clamp width of pixel crop", () => {
+    const { result } = renderHook(() => useCrop({ width: 600, height: 400 }));
+
+    act(() => result.current.changePixelCrop({ unit: "px", x: 0, y: 100, width: 700, height: 200 }));
+    expect(result.current.pixelCrop).toEqual({ unit: "px", x: 0, y: 100, width: 600, height: 200 });
+  });
+
+  it("should clamp height of pixel crop", () => {
+    const { result } = renderHook(() => useCrop({ width: 600, height: 400 }));
+
+    act(() => result.current.changePixelCrop({ unit: "px", x: 0, y: 100, width: 300, height: 400 }));
+    expect(result.current.pixelCrop).toEqual({ unit: "px", x: 0, y: 100, width: 300, height: 300 });
   });
 
   it("should sync percent crop and pixel crop", () => {
