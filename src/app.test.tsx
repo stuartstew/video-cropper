36ac6547295eea365f42a6ada5f4ca74fd83b56b
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-08-08 22:31:04 +0900

    test: mock `useMenu` in test

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,6 +1,10 @@
-import { describe, it } from "vitest";
+import { describe, it, vi } from "vitest";
 import { render } from "@/test-utils";
 import App from "./app";
+
+vi.mock("./hooks/use-menu", () => ({
+  useMenu: vi.fn(),
+}));
 
 describe("App", () => {
   it("renders without crashing", () => {
