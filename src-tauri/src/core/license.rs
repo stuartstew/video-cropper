6ee37a4e17a965194e013058b9effe25dbb548b8
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-08-12 23:08:02 +0900

    chore: move licenses directory into src-tauri

--- src-tauri/src/core/license.rs
+++ src-tauri/src/core/license.rs
@@ -36,7 +36,7 @@ pub struct FrontendLicense {
 #[command]
 #[allow(clippy::needless_pass_by_value, reason = "needed")]
 pub fn read_frontend_licenses(app: AppHandle) -> Result<Vec<FrontendLicense>, Error> {
-    let licenses_path = app.path().resource_dir()?.join("_up_/licenses/frontend-licenses.json");
+    let licenses_path = app.path().resource_dir()?.join("licenses/frontend-licenses.json");
     let file = File::open(licenses_path)?;
     let reader = BufReader::new(file);
 
