0871c7c0d528efeeefad26094611b3fb2e81c553
stuartstew <146310891+stuartstew@users.noreply.github.com>
2026-08-14 15:55:52 +0900

    test: mock tauri api

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,23 @@
-import { describe, it } from "vitest";
+import { randomFillSync } from "node:crypto";
+import { mockIPC } from "@tauri-apps/api/mocks";
+import { beforeAll, describe, it } from "vitest";
 import { render } from "@/test-utils";
 import App from "./app";
 
+beforeAll(() => {
+  Object.defineProperty(window, "crypto", {
+    value: {
+      // biome-ignore  lint/suspicious/noExplicitAny: boilerplate
+      getRandomValues: (buffer: any) => {
+        return randomFillSync(buffer);
+      },
+    },
+  });
+});
+
 describe("App", () => {
   it("renders without crashing", () => {
+    mockIPC(() => {});
     render(<App />);
   });
 });
